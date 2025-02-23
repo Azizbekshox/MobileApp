c2c5f3c304757c26f22f58676d9a2a88224f0171
Azizbekshox <azizbekshox18@gmail.com>
2025-02-23 23:37:59 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,77 +1,20 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:trading_app/screens/home_screen.dart';
 
 void main() {
-  runApp(TradingApp());
+  runApp(const MyApp());
 }
 
-class TradingApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: TradingScreen(),
-    );
-  }
-}
-
-class TradingScreen extends StatefulWidget {
-  @override
-  _TradingScreenState createState() => _TradingScreenState();
-}
-
-class _TradingScreenState extends State<TradingScreen> {
-  double price = 0.0;
-  bool isLoading = false;
-
-  Future<void> fetchPrice() async {
-    setState(() {
-      isLoading = true;
-    });
-
-    final response = await http.get(Uri.parse('https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT'));
-    
-    if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-      setState(() {
-        price = double.parse(data['price']);
-        isLoading = false;
-      });
-    } else {
-      setState(() {
-        isLoading = false;
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Crypto Price Checker")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Bitcoin narxi:",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            isLoading
-                ? CircularProgressIndicator()
-                : Text(
-                    "\$${price.toStringAsFixed(2)}",
-                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                  ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: fetchPrice,
-              child: Text("Narxni yangilash"),
-            ),
-          ],
-        ),
-      ),
+      title: 'Trading App',
+      theme: ThemeData.dark(),
+      home: const HomeScreen(),
     );
   }
 }
